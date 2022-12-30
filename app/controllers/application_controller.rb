b5cf8b04719775d126c8b34e6ed82a749040317c
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-12-30 21:58:21 +0200

    configure permitter parametters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, roles: [])}
+    end
 end
