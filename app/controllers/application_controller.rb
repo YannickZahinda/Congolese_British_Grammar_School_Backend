5d96ce48dd12203c3ff16ddbe1603b750bb458f5
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2023-01-27 09:10:38 +0200

    add authorization to admin, denie others

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from CanCan::AccessDenied do |exception| 
     render json: { warning: exception, status: 'authorization_failed' }
   end
